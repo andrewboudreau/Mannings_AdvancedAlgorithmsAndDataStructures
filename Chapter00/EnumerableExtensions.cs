697c15d223dfda399208ebb5636c0220a4cae017
Andrew Boudreau <andrewboudreau@gmail.com>
2022-07-02 11:21:04 -0500

    fix: remove copy/paste namespaces

--- Chapter00/EnumerableExtensions.cs
+++ Chapter00/EnumerableExtensions.cs
@@ -1,26 +1,23 @@
 ﻿using System.Collections;
 
-namespace Day00
+public static class EnumerableExtensions
 {
-    public static class EnumerableExtensions
+    public static int ToInt32(this string bits)
+        => ToInt32(bits.Select(x => x == '1'));
+
+    public static int ToInt32(this IEnumerable<int> bits)
+        => ToInt32(bits.Select(x => x > 0));
+
+    public static int ToInt32(this IEnumerable<bool> bools)
+        => ToInt32(new BitArray(bools.Reverse().ToArray()));
+
+    public static int ToInt32(this BitArray bits)
     {
-        public static int ToInt32(this string bits)
-            => ToInt32(bits.Select(x => x == '1'));
-
-        public static int ToInt32(this IEnumerable<int> bits)
-            => ToInt32(bits.Select(x => x > 0));
-
-        public static int ToInt32(this IEnumerable<bool> bools)
-            => ToInt32(new BitArray(bools.Reverse().ToArray()));
-
-        public static int ToInt32(this BitArray bits)
-        {
-            var bytes = new int[1];
-            bits.CopyTo(bytes, 0);
-            return bytes[0];
-        }
-
-        public static IEnumerable<int> Split(this IEnumerable<string> source, char split)
-            => source.SelectMany(x => x.Split(split), (a, b) => int.Parse(b));
+        var bytes = new int[1];
+        bits.CopyTo(bytes, 0);
+        return bytes[0];
     }
+
+    public static IEnumerable<int> Split(this IEnumerable<string> source, char split)
+        => source.SelectMany(x => x.Split(split), (a, b) => int.Parse(b));
 }
